8cbf7e4457e78a3824662c488a65b7d63346f903
FairyWorks Digital <fairy-works@users.noreply.github.com>
2025-10-28 04:53:02 +0000

    attempt to use netlify form autodetection

--- src/components/blocks/FormBlock/index.tsx
+++ src/components/blocks/FormBlock/index.tsx
@@ -43,7 +43,8 @@ export default function FormBlock(props) {
             id={elementId}
             onSubmit={handleSubmit}
             ref={formRef}
-            data-sb-field-path= {fieldPath}
+            data-sb-field-path={fieldPath}
+            netlify="true"
         >
             <div
                 className={classNames('w-full', 'flex', 'flex-wrap', 'gap-8', mapStyles({ justifyContent: styles?.self?.justifyContent ?? 'flex-start' }))}
