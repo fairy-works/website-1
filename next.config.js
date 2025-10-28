dbde9e9396b6fe33b13b2c886940044e0eb8c7c7
FairyWorks Digital <fairy-works@users.noreply.github.com>
2025-10-28 09:20:53 +0000

    allow origin: preview server

--- next.config.js
+++ next.config.js
@@ -8,7 +8,8 @@ const nextConfig = {
     trailingSlash: true,
     reactStrictMode: true,
     allowedDevOrigins: [
-        '192.168.1.84'
+        '192.168.1.84',
+        'devserver-dev--lambent-pie-62f7f2.netlify.app'
     ]
 };
 
